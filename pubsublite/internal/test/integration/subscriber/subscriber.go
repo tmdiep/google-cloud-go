0695a05b549d5b20adce99d76dbc7a0f6edd840d
tmdiep <tmdiep@google.com>
2021-09-05 05:16:51 -0400

    Subscriber - validate stuck partitions

--- pubsublite/internal/test/integration/subscriber/subscriber.go
+++ pubsublite/internal/test/integration/subscriber/subscriber.go
@@ -18,11 +18,11 @@ import (
 	"flag"
 	"fmt"
 	"log"
-	"sync/atomic"
+	"runtime"
+	"sync"
 	"time"
 
 	"cloud.google.com/go/pubsub"
-	"cloud.google.com/go/pubsublite/internal/test"
 	"cloud.google.com/go/pubsublite/internal/test/integration"
 	"cloud.google.com/go/pubsublite/internal/wire"
 	"cloud.google.com/go/pubsublite/pscompat"
@@ -30,10 +30,80 @@ import (
 )
 
 var (
-	printInterval = flag.Int("print_interval", 100, "print status every n-th message sent/received")
-	verbose       = flag.Bool("verbose", false, "whether to log verbose messages")
-	ackDelay      = flag.Duration("ack_delay", 0, "sleep before acking messages")
+	printInterval  = flag.Int("print_interval", 100, "print status every n-th message sent/received")
+	verbose        = flag.Bool("verbose", false, "whether to log verbose messages")
+	ackDelay       = flag.Duration("ack_delay", 0, "sleep before acking messages")
+	receiveTimeout = flag.Duration("receive_timeout", 2*time.Minute, "fail if messages not received within this duration for a partition")
 )
+
+type receiveCounter struct {
+	count      int64
+	partitions map[int]time.Time
+	mu         sync.Mutex
+}
+
+func newReceiveCounter(numPartitions int) *receiveCounter {
+	c := &receiveCounter{
+		count:      0,
+		partitions: make(map[int]time.Time),
+	}
+	now := time.Now()
+	for i := 0; i < numPartitions; i++ {
+		c.partitions[i] = now
+	}
+	return c
+}
+
+func (r *receiveCounter) OnMsgReceived(p int) int64 {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
+	r.count++
+	r.partitions[p] = time.Now()
+	return r.count
+}
+
+func (r *receiveCounter) checkReceived() {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
+	now := time.Now()
+	timeout := false
+	for _, t := range r.partitions {
+		if now.Sub(t) > *receiveTimeout {
+			timeout = true
+			break
+		}
+	}
+	if !timeout {
+		return
+	}
+
+	fmt.Println("------------------------------------------------------------")
+	buf := make([]byte, 1<<16)
+	runtime.Stack(buf, true)
+	fmt.Printf("%s\n", buf)
+	fmt.Println("------------------------------------------------------------")
+
+	for p, t := range r.partitions {
+		elapsed := now.Sub(t)
+		suffix := ""
+		if elapsed > *receiveTimeout {
+			suffix = " ***"
+		}
+		log.Printf("%d: %v%s", p, now.Sub(t), suffix)
+	}
+	log.Fatal("Timed out receiving messages")
+}
+
+func (r *receiveCounter) Poll() {
+	go func() {
+		for true {
+			r.checkReceived()
+			time.Sleep(10 * time.Second)
+		}
+	}()
+}
 
 const maxPrintMsgLen = 70
 
@@ -46,18 +116,15 @@ func truncateMsg(msg string) string {
 
 // subscriber contains a wire subscriber with message validators.
 type subscriber struct {
-	Subscription      wire.SubscriptionPath
-	Sub               *pscompat.SubscriberClient
-	OrderingValidator *test.OrderingReceiver
-	DuplicateDetector *test.DuplicateMsgDetector
-	receiveCount      int64
+	Subscription wire.SubscriptionPath
+	Sub          *pscompat.SubscriberClient
+	counter      *receiveCounter
 }
 
-func newSubscriber(ctx context.Context, harness *integration.TestHarness, subscription wire.SubscriptionPath, group *errgroup.Group) *subscriber {
+func newSubscriber(ctx context.Context, harness *integration.TestHarness, subscription wire.SubscriptionPath, partitions int, group *errgroup.Group) *subscriber {
 	s := &subscriber{
-		Subscription:      subscription,
-		OrderingValidator: test.NewOrderingReceiver(),
-		DuplicateDetector: test.NewDuplicateMsgDetector(),
+		Subscription: subscription,
+		counter:      newReceiveCounter(partitions),
 	}
 
 	sub := harness.StartSubscriber(subscription)
@@ -68,29 +135,24 @@ func newSubscriber(ctx context.Context, harness *integration.TestHarness, subscr
 		log.Fatalf("%s: stopped with error: %v", subscription, err)
 		return err
 	})
+	s.counter.Poll()
 
 	return s
 }
 
 func (s *subscriber) onReceive(ctx context.Context, msg *pubsub.Message) {
 	msg.Ack()
-	count := atomic.AddInt64(&s.receiveCount, 1)
+	m, err := pscompat.ParseMessageMetadata(msg.ID)
+	if err != nil {
+		log.Fatalf("Failed to parse metadata %q: %v", msg.ID, err)
+	}
+	count := s.counter.OnMsgReceived(m.Partition)
 	if *verbose || count%int64(*printInterval) == 0 {
 		log.Printf("Received: offset=%s, data=%s, published=%v, count=%d", msg.ID, truncateMsg(string(msg.Data)), msg.PublishTime, count)
 	}
 	if *ackDelay > 0 {
 		time.Sleep(*ackDelay)
 	}
-	/*
-		// Ordering and duplicate validation.
-		if err := s.OrderingValidator.Receive(data, key); err != nil {
-			log.Fatalf("%s: %v", s.Subscription, err)
-		}
-		s.DuplicateDetector.Receive(data, offset)
-		if s.DuplicateDetector.HasReceiveDuplicates() {
-			log.Fatalf("%s: %s", s.Subscription, s.DuplicateDetector.Status())
-		}
-	*/
 }
 
 func main() {
@@ -100,7 +162,7 @@ func main() {
 	group, gctx := errgroup.WithContext(context.Background())
 	var subscribers []*subscriber
 	for _, subscription := range harness.Subscriptions {
-		subscribers = append(subscribers, newSubscriber(gctx, harness, subscription, group))
+		subscribers = append(subscribers, newSubscriber(gctx, harness, subscription, harness.TopicPartitionCount, group))
 	}
 	group.Wait()
 }
